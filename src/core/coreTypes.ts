ae72373f4312ca62b9558def3678a5f97b2edd9e
Michael Pu <michael.pu123@gmail.com>
2020-06-14 13:06:30 -0400

    Fix linting errors, undo running transformer on core files

--- src/core/coreTypes.ts
+++ src/core/coreTypes.ts
@@ -1,33 +1,44 @@
+import { IExpressRequest, IExpressResponse, IResponse, IRequest, HttpMethod } from "./types"
+import { NextFunction } from "../../example/node_modules/@types/express"
+
 // middleware type?
-export type MiddlewareFunction = (...middleware: Handler[]) => {
-    middleware: Handler[];
-    subendpoints: SubendpointsFunction;
-    endpoints: EndpointsFunction;
-};
-export type SubendpointsFunction = (...subendpoints: EndpointsBuilderResult[]) => {
-    middleware: Handler[];
-    subendpoints: EndpointsBuilderResult[];
-    endpoints: EndpointsFunction;
-};
-export type EndpointsFunction = (endpoints: Record<string, EndpointMethod<unknown, any>>) => EndpointsBuilderResult;
+export type MiddlewareFunction = (
+  ...middleware: Handler[]
+) => {
+  middleware: Handler[]
+  subendpoints: SubendpointsFunction
+  endpoints: EndpointsFunction
+}
+export type SubendpointsFunction = (
+  ...subendpoints: EndpointsBuilderResult[]
+) => {
+  middleware: Handler[]
+  subendpoints: EndpointsBuilderResult[]
+  endpoints: EndpointsFunction
+}
+export type EndpointsFunction = (endpoints: Record<string, EndpointMethod<unknown, any>>) => EndpointsBuilderResult
 export type EndpointsBuilder = () => {
-    middleware: MiddlewareFunction;
-    subendpoints: SubendpointsFunction;
-    endpoints: EndpointsFunction;
-};
+  middleware: MiddlewareFunction
+  subendpoints: SubendpointsFunction
+  endpoints: EndpointsFunction
+}
 export type EndpointsBuilderResult = () => {
-    middleware: Handler[];
-    subendpoints: EndpointsBuilderResult[];
-    endpoints: Record<string, EndpointMethod<unknown, IRequest>>;
-};
+  middleware: Handler[]
+  subendpoints: EndpointsBuilderResult[]
+  endpoints: Record<string, EndpointMethod<unknown, IRequest>>
+}
 export type IHandlerOptions<Res = {}, Req extends IRequest = IRequest> = {
-    req: IExpressRequest<Req>;
-    res: IExpressResponse<IResponse<Res | undefined>>;
-    next: NextFunction;
-};
-export type Handler<Res = {}, Req extends IRequest = IRequest> = (req: IExpressRequest<Req>, res: IExpressResponse<IResponse<Res | undefined>>, next: NextFunction) => Promise<void> | void;
+  req: IExpressRequest<Req>
+  res: IExpressResponse<IResponse<Res | undefined>>
+  next: NextFunction
+}
+export type Handler<Res = {}, Req extends IRequest = IRequest> = (
+  req: IExpressRequest<Req>,
+  res: IExpressResponse<IResponse<Res | undefined>>,
+  next: NextFunction
+) => Promise<void> | void
 export type EndpointMethod<Res extends unknown, Req extends IRequest = IRequest> = {
-    method: HttpMethod;
-    path: string;
-    handlers: Handler<Res, Req>[];
-};
+  method: HttpMethod
+  path: string
+  handlers: Handler<Res, Req>[]
+}
--- src/core/types.ts
+++ src/core/types.ts
@@ -1,47 +1,99 @@
-export type MainHttpMethod = "GET" | "POST" | "PUT" | "DELETE";
-export type OtherHttpMethod = "CHECKOUT" | "COPY" | "HEAD" | "LOCK" | "MERGE" | "MKACTIVITY" | "MKCOL" | "MOVE" | "M-SEARCH" | "NOTIFY" | "OPTIONS" | "PATCH" | "PURGE" | "REPORT" | "SEARCH" | "SUBSCRIBE" | "TRACE" | "UNLOCK" | "UNSUBSCRIBE";
-export type HttpMethod = MainHttpMethod | OtherHttpMethod;
+import { Request, Response } from "express"
+
+export type MainHttpMethod = "GET" | "POST" | "PUT" | "DELETE"
+export type OtherHttpMethod =
+  | "CHECKOUT"
+  | "COPY"
+  | "HEAD"
+  | "LOCK"
+  | "MERGE"
+  | "MKACTIVITY"
+  | "MKCOL"
+  | "MOVE"
+  | "M-SEARCH"
+  | "NOTIFY"
+  | "OPTIONS"
+  | "PATCH"
+  | "PURGE"
+  | "REPORT"
+  | "SEARCH"
+  | "SUBSCRIBE"
+  | "TRACE"
+  | "UNLOCK"
+  | "UNSUBSCRIBE"
+
+export type HttpMethod = MainHttpMethod | OtherHttpMethod
 export interface IRequest<Params = Record<string, string>, Body = Record<string, unknown>, Query = {}> {
-    type: HttpMethod;
-    params: Params;
-    body: Body;
-    query: Query;
+  type: HttpMethod
+  params: Params
+  body: Body
+  query: Query
 }
+
 export interface IResponse<Body> {
-    data?: Body;
-    success: boolean;
-    status: ErrorStatus;
-    error?: string;
+  data?: Body
+  success: boolean
+  status: ErrorStatus
+  error?: string
 }
-export type IExpressRequest<Req extends IRequest = IRequest> = Request<Req["params"], unknown, Req["body"], Req["query"]>;
-export type IExpressResponse<Res extends IResponse<unknown>> = Response<Res>;
-export type IGetRequest<Params = Record<string, string>, Query = {}> = IRequest<Params, {}, Query>;
-export type IPostRequest<Params extends Record<string, string>, Body extends Record<string, unknown>> = IRequest<Params, Body, {}>;
-export type IPutRequest<Params extends Record<string, string>, Body extends Record<string, unknown>> = IRequest<Params, Body, {}>;
-export type IDeleteRequest<Params extends Record<string, string>, Body extends Record<string, unknown>> = IRequest<Params, Body, {}>;
+
+export type IExpressRequest<Req extends IRequest = IRequest> = Request<
+  Req["params"],
+  unknown,
+  Req["body"],
+  Req["query"]
+>
+export type IExpressResponse<Res extends IResponse<unknown>> = Response<Res>
+
+export type IGetRequest<Params = Record<string, string>, Query = {}> = IRequest<Params, {}, Query>
+export type IPostRequest<Params extends Record<string, string>, Body extends Record<string, unknown>> = IRequest<
+  Params,
+  Body,
+  {}
+>
+export type IPutRequest<Params extends Record<string, string>, Body extends Record<string, unknown>> = IRequest<
+  Params,
+  Body,
+  {}
+>
+export type IDeleteRequest<Params extends Record<string, string>, Body extends Record<string, unknown>> = IRequest<
+  Params,
+  Body,
+  {}
+>
+
 export type IExpressEndpointHandlerOptions<Req extends IRequest = IRequest, Res = {}> = {
-    req: IExpressRequest<Req>;
-    res: IExpressResponse<IResponse<Res>>;
-    error: IErrorStatusMethods;
-};
-export type IExpressEndpointHandler<Req extends IRequest = IRequest, Res = {}> = (options: IExpressEndpointHandlerOptions<Req, Res>) => Promise<Res | IErrorStatus>;
-export enum ErrorStatus {
-    BAD_REQUEST = 400,
-    UNAUTHORIZED = 401,
-    FORBIDDEN = 403,
-    NOT_FOUND = 404,
-    INTERNAL_ERROR = 500
+  req: IExpressRequest<Req>
+  res: IExpressResponse<IResponse<Res>>
+  error: IErrorStatusMethods
 }
+
+export type IExpressEndpointHandler<Req extends IRequest = IRequest, Res = {}> = (
+  options: IExpressEndpointHandlerOptions<Req, Res>
+) => Promise<Res | IErrorStatus>
+
+export enum ErrorStatus {
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  INTERNAL_ERROR = 500
+}
+
 export type IErrorStatus = {
-    isError: true;
-    type: ErrorStatus;
-    error?: Error;
-};
+  isError: true
+  type: ErrorStatus
+  error?: Error
+}
+
 export type IErrorStatusMethods = {
-    forbidden: (err?: Error) => Promise<IErrorStatus>;
-    unauthorized: (err?: Error) => Promise<IErrorStatus>;
-    badRequest: (err?: Error) => Promise<IErrorStatus>;
-    notFound: (err?: Error) => Promise<IErrorStatus>;
-    internalError: (err?: Error) => Promise<IErrorStatus>;
-};
-export type IRequestValidation<Req extends IRequest<{}, {}, {}>> = (data: IExpressRequest<Req>) => boolean | [boolean, string];
+  forbidden: (err?: Error) => Promise<IErrorStatus>
+  unauthorized: (err?: Error) => Promise<IErrorStatus>
+  badRequest: (err?: Error) => Promise<IErrorStatus>
+  notFound: (err?: Error) => Promise<IErrorStatus>
+  internalError: (err?: Error) => Promise<IErrorStatus>
+}
+
+export type IRequestValidation<Req extends IRequest<{}, {}, {}>> = (
+  data: IExpressRequest<Req>
+) => boolean | [boolean, string]
